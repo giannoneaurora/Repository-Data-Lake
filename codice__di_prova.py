310823930834118cb119c334285f4351f3b4a84b
giannoneaurora <giannoneaurora972@gmail.com>
2024-06-20 16:24:32 +0200

    Update codice__di_prova.py
    Aggiunta del collegamento al server redis

--- codice__di_prova.py
+++ codice__di_prova.py
@@ -193,3 +193,8 @@ def read_messages(user, contact):
     chat_key = f'chat:{user}:{contact}'
     messages = redis_client.lrange(chat_key, 0, -1)
     return messages
+r = redis.Redis(host='redis-19153.c15.us-east-1-2.ec2.redns.redis-cloud.com',
+                    port=19153, db=0, charset="utf-8", decode_responses=True,
+                    password="FutfU3TjZa2sP4ne24aaVExF8A6oVn81")
+
+print(r.ping())
