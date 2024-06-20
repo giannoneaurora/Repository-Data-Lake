119822e4f137780df00e1f8969c8a859ce373eed
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-06-20 14:28:06 +0200

    Created search user function

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import redis
-import json
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
--- user_interface.py
+++ user_interface.py
@@ -14,17 +14,21 @@ def register_user(client):
     
 # Si potrebbe aggiungere lo stato, online/offline
 def create_hash_table(username, hashed_password):
-    user_data = {"Username:": username, "Hashed-Password:": hashed_password, "DoNotDisturd": False}
+    user_data = {"Username:": username, "Hashed-Password:": hashed_password, "DoNotDisturb": False}
 
 
 def hash_password(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def search_user(searched_user):
+    users = [u for u in redis_client.keys() if searched_user in str(u)]
     
+    return users
     
 def save_user_info(user_id, user_info):
-    client.hmset(user_id, user_info)
+    redis_client.hmset(user_id, user_info)
 
 # Funzione per recuperare le informazioni dell'utente
 def get_user_info(user_id):
-    return client.hgetall(user_id)
-    return True
+    return redis_client.hgetall(user_id)
+    
