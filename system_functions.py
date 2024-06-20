052e65f58f046cf9b147d1938a36d4da73f8dc64
Deker3679 <roberto.goldin@itsrizzoli.it>
2024-06-20 17:34:45 +0200

    Updated register_user function and create_user

--- system_functions.py
+++ system_functions.py
@@ -1,5 +1,23 @@
 import redis
 import time
+import password_hashing as ph
+
+def register_user(client):
+    new_username = str(input('Scrivi il tuo nome utente: '))
+    if client.exists(new_username):
+        return False
+    while True:
+        new_password = str(input('Crea la tua password: '))
+        if ph.check_password(new_password):
+            print('Password corretta!')
+            break
+    hashed_password = ph.hash_password(new_password)
+    new_user_mapping = create_mapping(new_username, hashed_password)
+    create_user(client, new_username, new_user_mapping)
+    
+
+def create_user(client, username, new_user_mapping):
+    pass
 
 def create_mapping(username, hashed_password):
     user_mapping = {"Username": username, "Hashed-Password": hashed_password, "DoNotDisturb": "OFF"}
