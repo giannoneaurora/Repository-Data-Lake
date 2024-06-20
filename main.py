8c261a21e9e1c781d123eea7b7b10df8b0da85cb
Deker3679 <roberto.goldin@itsrizzoli.it>
2024-06-20 15:23:21 +0200

    Testing connection to Redis server

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import redis
+import redis as rd
 
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+host_name = 'localhost'
+port_number = 6379
+db_number = 0
 
+redis_client = rd.StrictRedis(host = host_name, port = port_number, db = db_number, decode_responses=True)
+
+username_test = 'TEST'
+
+dummy_hash_password = "b'$2b$12$2Sy/LDvtWMHrmybvTpG4IuhtRiEgYvF3u58CSwyv5SAMOT.c7ECNW'"
+
+redis_client.hset('Test_User', username_test, mapping = {'Username': username_test,'Hashed-Password': dummy_hash_password, 'DoNotDisturb': False})
 
