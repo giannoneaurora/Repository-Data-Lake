59c7c01e894fa20d9ab8c1990c304788b0171960
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-06-29 12:00:57 +0200

    Testing create_chat function

--- program_test.py
+++ program_test.py
@@ -1,8 +1,15 @@
-import redis
+import redis as rd
 import system_functions as sf
-import random
-import time
+import client_server_connection as csc
+from main import users_list
+import chat_functions as cf
+import user_interface as ui
 
+# Questo file serve solo per testare la connessione al server e il funzionamento del programma
+
+redis_client = csc.get_client().redis_client
+
+redis_client.ping()
 
 usernames_test = ['TEST1','TEST2','TEST3','TEST4','TEST5']
 
@@ -16,13 +23,25 @@ dummy_messages = ['Hi T1!',
 
 dummy_mappings_list = []
 for item in zip(usernames_test,dummy_hashed_passwords):
-    dummy_mappings_list.append(sf.create_mapping(item[0],item[1]))
-
-for user in usernames_test:
+    dummy_mappings_list.append(sf.create_user_mapping(item[0],item[1]))
+ 
+for u, map in zip(usernames_test,dummy_mappings_list):
     try:
-  # Use username_test as the field name itself
-        redis_client.hset(f'User:{user}', mapping = {'Username': user, 'Hashed-Password': dummy_hash_password, 'DoNotDisturb': 'OFF'})
+        redis_client.hset(f'User:{u}', mapping = map)
     except Exception as e:
-        print(f"An Error has occurred: {e}")  # Print any errors encountered
+        print(f"An Error has occurred: {e}")  
+
+# Usa per controllare tutta la lista di utenti presenti nel server
+# users_list()
+
+# cf.create_chat(usernames_test[0], usernames_test[1], redis_client)
+
+def search_user(searched_user, client):
+    u_pattern = 'User:*'
+    users = [u for u in client.keys(pattern = u_pattern, decode_response = True) if searched_user in u[:]]
+    return users
+
+search_user('T', redis_client)
+
 
 
